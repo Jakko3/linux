5282a3f1e8437f7a33469ab600ce31b49fb9df70
Jakob Hauser <jahau@rocketmail.com>
2022-12-20 03:34:14 +0100

    power: supply: rt5033_battery: Adopt status property from charger
    The rt5033-battery fuelgauge can't get a status by itself. The rt5033-charger
    can, so let's get this value.
    
    The status property of the rt5033-charger, on the other hand, doesn't seem to
    reach a status "full" in practice. In the downstream Android driver this state
    gets set by end-of-charge IRQ handler [1][2]. In the rt5033-battery driver,
    status "full" can be set when reaching a capacity of 100 %.
    
    [1] https://github.com/msm8916-mainline/linux-downstream/blob/GT-I9195I/drivers/battery/rt5033_charger.c#L1082
    [2] https://github.com/msm8916-mainline/linux-downstream/blob/GT-I9195I/drivers/battery/rt5033_charger.c#L669-L670
    
    Signed-off-by: Jakob Hauser <jahau@rocketmail.com>

--- drivers/power/supply/rt5033_battery.c
+++ drivers/power/supply/rt5033_battery.c
@@ -22,6 +22,33 @@ static int rt5033_battery_get_capacity(struct i2c_client *client)
 	return msb;
 }
 
+static int rt5033_battery_get_status(struct i2c_client *client)
+{
+	struct rt5033_battery *battery = i2c_get_clientdata(client);
+	struct power_supply *charger;
+	union power_supply_propval val;
+	int capacity;
+	int ret;
+
+	capacity = rt5033_battery_get_capacity(battery->client);
+
+	if (capacity == 100)
+		return POWER_SUPPLY_STATUS_FULL;
+
+	charger = power_supply_get_by_name("rt5033-charger");
+	if (!charger)
+		return -ENODEV;
+
+	ret = power_supply_get_property(charger, POWER_SUPPLY_PROP_STATUS, &val);
+	if (ret) {
+		power_supply_put(charger);
+		return POWER_SUPPLY_STATUS_UNKNOWN;
+	}
+
+	power_supply_put(charger);
+	return val.intval;
+}
+
 static int rt5033_battery_get_present(struct i2c_client *client)
 {
 	struct rt5033_battery *battery = i2c_get_clientdata(client);
@@ -81,6 +108,9 @@ static int rt5033_battery_get_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_PRESENT:
 		val->intval = rt5033_battery_get_present(battery->client);
 		break;
+	case POWER_SUPPLY_PROP_STATUS:
+		val->intval = rt5033_battery_get_status(battery->client);
+		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
 		val->intval = rt5033_battery_get_capacity(battery->client);
 		break;
@@ -95,6 +125,7 @@ static enum power_supply_property rt5033_battery_props[] = {
 	POWER_SUPPLY_PROP_VOLTAGE_AVG,
 	POWER_SUPPLY_PROP_VOLTAGE_OCV,
 	POWER_SUPPLY_PROP_PRESENT,
+	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_CAPACITY,
 };
 
